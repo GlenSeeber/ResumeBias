35809a452acc5ab80a1766ee842117f5281f81fe
Glen Seeber <gseeber5@gmail.com>
2022-01-22 20:25:45 -0800

    Add func to iterate resumes sendResume() will read the first resume (resume0.json), paste its contents onto input.json, then run stamp(). stamp() will paste a random name and Identity over the resume json. sendResume() will then increase the resumeCount int, restarting this whole process, but using resume1.json instead of 0. . I am going to add a way to send the json (output.json) to frontend/client side, but it may be of use to eventually add a try statement to prevent the server from crashing when it runs out of resumes to send.

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ def liMaker(string):
     li = list(string.split(', '))
     return li
 
+def jsonRead(myPath):
+    f = open(myPath, 'r')
+    myJson = json.load(f)
+    f.close()
+    return myJson
+
+def jsonWrite(myPath, dumpContent):
+    f = open(myPath, "w")
+    json.dump(dumpContent, f)
+    f.close()
+
 def randomId():
     # 2 genders * 3 races = 6 options
     # pick a random number 1-6
@@ -48,12 +59,11 @@ def fillName(name, gender, race):
     # output.json
 
     # open template file
-    f = open("resume.json", "r")
+    f = open("input.json", "r")
     myJson = json.load(f)
     f.close()
 
     # change whatever values you want
-    print(name)
     myJson["Name"] = name
     myJson["Gender"] = gender
     myJson["Race"] = race
@@ -63,7 +73,7 @@ def fillName(name, gender, race):
     json.dump(myJson, f)
     f.close()
 
-def coverUp():
+def stamp():
     # covers up the top of a resume with a fake name and identity
     id = randomId()
 
@@ -73,5 +83,30 @@ def coverUp():
 
     fillName(name, id[0], id[1])
 
+def sendResume():
+    global resumeCount
+    #open 
+    myJson = jsonRead('resume'+str(resumeCount)+'.json')
+
+    jsonWrite('input.json', myJson)
+
+    stamp()
+
+    #send the file to frontend
+    #confirm it has been recieved (somehow)
+
+    resumeCount += 1
+
 #program
 
+resumeCount = 0
+
+run = True
+
+while run:
+    # change condition to:
+    # "if a request has been recieved from frontend"
+    if True:
+        sendResume()
+    # change to an if statement, if "client is unresponsive", then quit, or something
+    run = False
