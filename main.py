cd6c07aa73cb9b92d227ccb5197be64b90fbd431
Glen Seeber <gseeber5@gmail.com>
2022-01-22 18:06:01 -0800

    Add name picker func randomName() picks a random name from the 5 available in a given identity. I also took code from the 'program' part of the code, and moved it into a function called fillName(). Now I just need to get all the parts to work together

--- main.py
+++ main.py
@@ -3,38 +3,61 @@
 # 
 # Name (str), Age (int), Skills [list], Experiences [list], Education [list], Additonal Notes (str)
 
+from inspect import GEN_CLOSED
 import random
 import shutil
 import json
 
-def randomizer():
+def randomId():
     # 2 genders * 3 races = 6 options
     # pick a random number 1-6
     # numbers correlate in this order:
     # White man, Black man, Latino man, White woman, Black woman, Latino Woman
     
-    r = random.randint(0, 5)
-    if (r <= 2):
+    myRand = random.randint(0, 5)
+    if (myRand <= 2):
         gender = "Masculine"
 
-    elif (r >= 3):
+    elif (myRand >= 3):
         gender = "Feminine"
-        r -= 3
+        myRand -= 3
 
     raceIndex = {0: "White", 1:"Black", 2:"Latino"}
-    race = raceIndex[r]
+    race = raceIndex[myRand]
 
+    return gender, race
 
+def randomName(gender, race):
+    # picks a random name based on the race and gender given by
+    # randomId()
 
-#open template file
-f = open("resume.json", "r")
-myJson = json.load(f)
-f.close()
+    myRand = random.randint(0, 4)
 
-#change whatever values you want
-myJson["name"] = "james"
+    f = open("names.json", "r")
+    myJson = json.load(f)
+    f.close()
 
-#open/create an output file, which will have actual values inputted
-f = open("output.json", "w")
-json.dump(myJson, f)
-f.close()
+    name = myJson.get(str(gender), {}).get(str(race), {})[myRand]
+
+    print(name)
+    return name
+
+def fillName(name):
+    # makes a copy of resume.json (the template), pastes values into it on
+    # output.json
+
+    # open template file
+    f = open("resume.json", "r")
+    myJson = json.load(f)
+    f.close()
+
+    # change whatever values you want
+    myJson["name"] = name
+
+    # open/create an output file, which will have actual values inputted
+    f = open("output.json", "w")
+    json.dump(myJson, f)
+    f.close()
+
+#program
+randomName("Feminine", "Latino")
